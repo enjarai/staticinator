9e71415b567109ee5edc0764bd6efe795d5e2bfd
enjarai <enjarai@trans.fish>
2025-08-04 11:04:37 +0200

    meowmeow

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+set -e
+
+FOLDER=$INPUT_FOLDER
+HOST=$INPUT_HOST
+TOKEN=$INPUT_TOKEN
+DEPLOY_ENDPOINT=$INPUT_DEPLOY_ENDPOINT
+
+echo "Compressing input"
+
+TMP_DIR=$(mktemp -d)
+tar -czvf "$TMP_DIR/archive.tar.gz" $FOLDER/*
+
+echo "Sending to deploy endpoint"
+
+curl -F "file=@$TMP_DIR/archive.tar.gz" -H "Token: $TOKEN" -H "Target-Host: $HOST" "$DEPLOY_ENDPOINT" -v
+
+echo "Deployment complete"
